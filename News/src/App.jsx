ef6646f9f757e95dde012af1d3d620afd5d5ffaf
PrimeSykuno <yogeshgoyal87454@gmail.com>
2025-12-10 18:35:05 +0530

    rg

--- News/src/App.jsx
+++ News/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/Navbartemp";
 import CategoryPage from "./pages/categorypage";
 
 function App() {
